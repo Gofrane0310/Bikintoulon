51b52ea78118e51d891b81aa4fb119ec2a143c84
BEJAOUI Gofrane <gofrane.bejaoui@isen.yncrea.fr>
2020-06-06 00:44:21 +0200

    Merge origin/master
    Conflicts:
    	public_html/css/css_general.css

--- public_html/js/gardeforme.js
+++ public_html/js/gardeforme.js
@@ -7,22 +7,17 @@ window.addEventListener("load", function() {
     window.document.querySelector("#btn_calculer").addEventListener("click", calculCal);
 });
 
-window.addEventListener("load", function() {
-    window.document.querySelector("#btn_calculer").addEventListener("click", calories);
-});
-
-function calories (nb_effort, nb_poids, nb_temps) {
-    let nb_calories;
+function calories (nb_cal) {
     let elH1 = window.document.querySelector('#formulaire');
-    elH1 = window.document.createElement('h1');
-    elH1.id = 'remuneration';
+    elH1 = window.document.createElement('p');
+    elH1.id = 'formulaire';
+
+   /* elH1.innerHTML = 'Une balade de ' + document.getElementById("form_simulateur").elements["nb_temps"].value + ' minutes vous a fait perdre ' + nb_cal + ' Kcal !';*/
+    elH1.appendChild(window.document.createTextNode('Une balade de ' + document.getElementById("form_simulateur").elements["nb_temps"].value + ' minutes vous a fait perdre ' + nb_cal + ' Kcal !'));
     window.document.querySelector('#recueilinfos').appendChild(elH1);
-    nb_calories = ((nb_effort*3,5*nb_poids)/200)*nb_temps;
-    elH1.innerHTML = 'Une balade de nb_temps minutes vous a fait perdre nb_calories Kcal !';
-    alert(nb_calories);
 }
 
 function calculCal() {
-    var cal = ((document.getElementById("form_simulateur").elements["nb_poids"].value * document.getElementById("form_simulateur").elements["nb_effort"].value*3,5)/200)*document.getElementById("form_simulateur").elements["nb_temps"].value;
-    alert(cal);
+    var cal = document.getElementById("form_simulateur").elements["nb_poids"].value * document.getElementById("form_simulateur").elements["nb_effort"].value*document.getElementById("form_simulateur").elements["nb_temps"].value*3.5/200;
+    calories(cal);
 }
